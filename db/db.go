008203d42312648adec7437f274b51b82ee02bd5
Yannick Foeillet <bzhtux@gmail.com>
2022-05-24 09:20:04 +0200

    feat(db) Upgrade db package to connect to PG

--- db/db.go
+++ db/db.go
@@ -6,23 +6,19 @@ import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-n
-func OpenDB(dsn string) *gorm.DB {
 
-	// databaseDriver := os.Getenv("DATABASE_DRIVER")
+func OpenDB(dsn string) *gorm.DB {
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("%s", err)
 	}
-	// if err := AutoMigrate(db); err != nil {
-	// 	panic(err)
-	// }
+
 	return db
 }
 
-func AutoMigrate(db *gorm.DB, database interface{}) error {
+func AutoMigrate(db *gorm.DB, database interface{}) {
 
-	return db.AutoMigrate(db, database).Error
+	db.AutoMigrate(database)
 
 }
