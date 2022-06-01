6b22ca30bed9cf412d39b2c412ae9298e6dc61fb
Yannick Foeillet <bzhtux@gmail.com>
2022-06-01 08:14:38 +0200

    feat(users) Add hashpassword and auth

--- users/users.go
+++ users/users.go
@@ -2,7 +2,6 @@ package users
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -10,11 +9,12 @@ import (
 )
 
 type User struct {
-	ID        uint `gorm:"primaryKey"`
-	Name      string
-	Password  string
-	Email     string `gorm:"unique"`
-	NickName  string
+	ID        uint   `gorm:"primaryKey"`
+	FirstName string `gorm:"not null" json:"firstname"`
+	LastName  string `gorm:"not null" json:"lastname"`
+	Password  string `gorm:"not null" json:"password"`
+	Email     string `gorm:"unique;index" json:"email"`
+	NickName  string `json:"nickname"`
 	Avatar    string
 	Valid     bool
 	CreatedAt time.Time
@@ -22,13 +22,21 @@ type User struct {
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
-func HashPassword(userpassword string) (string, error) {
+func HashPassword(userpassword string) string {
 	encrypt_pass, err := bcrypt.GenerateFromPassword([]byte(userpassword), 14)
 	if err != nil {
 		fmt.Println(err)
-		return "", err
 	}
-	return string(encrypt_pass), nil
+	return string(encrypt_pass)
+}
+
+func GetPasswordFromEmail(db *gorm.DB, email string) string {
+	var u = User{}
+	result := db.Where("Email = ?", email).First(&u)
+	if result.RowsAffected == 0 {
+		return ""
+	}
+	return u.Password
 }
 
 func ComparePassword(plain, hash string) bool {
@@ -36,7 +44,16 @@ func ComparePassword(plain, hash string) bool {
 	h_pass := []byte(hash)
 	err := bcrypt.CompareHashAndPassword(h_pass, p_pass)
 	if err != nil {
-		log.Println(err)
+		// log.Println("ComparePasswordError: ", err)
+		return false
+	}
+	return true
+}
+
+func UserExists(db *gorm.DB, email string) bool {
+	var u = User{}
+	result := db.Where("Email = ?", email).First(&u)
+	if result.RowsAffected == 0 {
 		return false
 	}
 	return true
@@ -44,31 +61,37 @@ func ComparePassword(plain, hash string) bool {
 
 func AddDefaultUser(db *gorm.DB) uint {
 	var user = User{}
-	anon := User{Name: "Anonymous", Password: "nimda", Email: "ano@nymous.org", NickName: "Anon", Valid: true}
-	result := db.Where("Name = ?", "Anonymous").First(&user)
+	anon := User{FirstName: "Anonymous", LastName: "RuleZ", Password: HashPassword("nimda"), Email: "ano@nymous.org", NickName: "Anon", Valid: true}
+	result := db.Where("Email = ?", "ano@nymous.org").First(&user)
 
 	if result.RowsAffected == 0 {
 		db.Create(&anon)
 		return anon.ID
 	}
 
-	db.Where("Name = ?", "Anonymous").First(&user)
-	fmt.Printf("User %s already exists\n", anon.Name)
+	db.Where("Email = ?", "ano@nymous.org").First(&user)
+	fmt.Printf("User %s already exists\n", anon.FirstName)
 	return user.ID
 
 }
 
-func AddNewUser(db *gorm.DB, usr *User) uint {
+func AddNewUser(db *gorm.DB, uFName, uLName, uPass, uEmail, uNick string) uint {
 	var user = User{}
-	result := db.Where("Name = ?", usr.Name).First(&user)
+	result := db.Where("Email = ?", uEmail).First(&user)
+
+	user.FirstName = uFName
+	user.LastName = uLName
+	user.Password = HashPassword(uPass)
+	user.Email = uEmail
+	user.NickName = uNick
 
 	if result.RowsAffected == 0 {
-		db.Create(&usr)
-		return usr.ID
+		db.Create(&user)
+		return user.ID
 	}
 
-	fmt.Printf("User %s already exists\n", usr.Name)
-	return usr.ID
+	fmt.Printf("User %s already exists\n", user.FirstName)
+	return user.ID
 }
 
 func GetUserByID(db *gorm.DB, id int) *User {
