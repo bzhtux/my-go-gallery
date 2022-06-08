02ac6ab146a7c23d669bffdab6a0a52cdb3bfa60
Yannick Foeillet <bzhtux@gmail.com>
2022-06-08 11:40:25 +0200

    fix(users) Remove duplicated functions after a hard merge

--- backend/users/users.go
+++ backend/users/users.go
@@ -3,10 +3,15 @@ package users
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"strconv"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
+
+	"github.com/bzhtux/my-go-gallery/backend/db"
 )
 
 type User struct {
@@ -23,14 +28,6 @@ type User struct {
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
-func HashPassword(userpassword string) string {
-	encrypt_pass, err := bcrypt.GenerateFromPassword([]byte(userpassword), 14)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return string(encrypt_pass)
-}
-
 func GetPasswordFromEmail(db *gorm.DB, email string) string {
 	var u = User{}
 	result := db.Where("Email = ?", email).First(&u)
@@ -41,33 +38,23 @@ func GetPasswordFromEmail(db *gorm.DB, email string) string {
 	}
 }
 
-func ComparePassword(plain, hash string) bool {
-	p_pass := []byte(plain)
-	h_pass := []byte(hash)
-	err := bcrypt.CompareHashAndPassword(h_pass, p_pass)
-	if err != nil {
+func UserExists(db *gorm.DB, email string) bool {
+	var u = User{}
+	result := db.Where("Email = ?", email).First(&u)
+	if result.RowsAffected == 0 {
 		return false
 	} else {
 		return true
 	}
 }
 
-func UserExists(db *gorm.DB, email string) bool {
-	var u = User{}
-	result := db.Where("Email = ?", email).First(&u)
-	if result.RowsAffected == 0 {
-		return false
-	}
-	return true
-}
-
-func HashPassword(userpassword string) (string, error) {
+func HashPassword(userpassword string) string {
 	encrypt_pass, err := bcrypt.GenerateFromPassword([]byte(userpassword), 14)
 	if err != nil {
 		fmt.Println(err)
-		return "", err
+		return ""
 	}
-	return string(encrypt_pass), nil
+	return string(encrypt_pass)
 }
 
 func ComparePassword(plain, hash string) bool {
@@ -77,8 +64,9 @@ func ComparePassword(plain, hash string) bool {
 	if err != nil {
 		log.Println(err)
 		return false
+	} else {
+		return true
 	}
-	return true
 }
 
 func AddDefaultUser(db *gorm.DB) uint {
@@ -97,43 +85,67 @@ func AddDefaultUser(db *gorm.DB) uint {
 
 }
 
-func AddNewUser(db *gorm.DB, uFName, uLName, uPass, uEmail, uNick string) uint {
+func AddNewUser(c *gin.Context) {
+	db := db.OpenDB()
 	var user = User{}
-	result := db.Where("Email = ?", uEmail).First(&user)
-
-	user.FirstName = uFName
-	user.LastName = uLName
-	user.Password = HashPassword(uPass)
-	user.Email = uEmail
-	user.NickName = uNick
-
-	if result.RowsAffected == 0 {
-		db.Create(&user)
-		return user.ID
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Status": "Can not register nes user:" + err.Error()})
+	} else {
+		result := db.Where("Email = ?", user.Email).First(&user)
+		if result.RowsAffected == 0 {
+			hpass := HashPassword(user.Password)
+			user.Password = hpass
+			db.Create(&user)
+			c.JSON(http.StatusOK, gin.H{"userID": user.ID})
+		} else {
+			c.JSON(http.StatusConflict, gin.H{"Status": user.Email + " already exists"})
+		}
 	}
-
-	fmt.Printf("User %s already exists\n", user.FirstName)
-	return user.ID
 }
 
-func GetUserByID(db *gorm.DB, id int) *User {
+func GetUserByID(c *gin.Context) {
+	userID := c.Params.ByName("uid")
+	intVal, _ := strconv.Atoi(userID)
+	// db := db.OpenDB()
+	db := db.Conn{}
 	var user = User{}
-	result := db.Where("ID = ?", id).First(&user)
-
+	result := db.DBConn.Where("ID = ?", intVal).First(&user)
 	if result.RowsAffected == 0 {
-		return nil
+		c.JSON(http.StatusNotFound, gin.H{"message": "No Username with id " + userID})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"firstname": user.FirstName, "lastname": user.LastName, "email": user.Email, "nickname": user.NickName})
 	}
-
-	return &user
 }
 
-func GetUserByID(db *gorm.DB, id int) *User {
+func AuthUser(c *gin.Context) {
 	var user = User{}
-	result := db.Where("ID = ?", id).First(&user)
-
-	if result.RowsAffected == 0 {
-		return nil
+	db := db.OpenDB()
+	err := c.BindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err})
 	}
+	dbPass := GetPasswordFromEmail(db, user.Email)
+	if !ComparePassword(user.Password, dbPass) {
+		c.JSON(http.StatusForbidden, gin.H{"Status": "Unauthorized"})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"Status": "Authorized - creating JWT"})
+	}
+}
 
-	return &user
+func DeleteUser(c *gin.Context) {
+	userID := c.Params.ByName("uid")
+	db := db.OpenDB()
+	var user = User{}
+	result := db.Where("ID = ?", userID).First(&user)
+	user_email := user.Email
+	if result.RowsAffected == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"Status": "User " + user_email + " does not exist!"})
+	} else {
+		now := time.Now()
+		new_email := user.Email + "." + now.String()
+		db.Model(&User{}).Where("ID = ? ", userID).Update("Email", new_email)
+		db.Delete(&user, userID)
+		c.JSON(http.StatusOK, gin.H{"Status": "User with ID " + userID + " was successfully deleted!"})
+	}
 }
