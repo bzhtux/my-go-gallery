5bf8c732b22ae2ab2d4ddc1e8d3cdcb42ff5f68d
Yannick Foeillet <bzhtux@gmail.com>
2022-06-08 11:44:38 +0200

    feat(main) Update start and routing func

--- backend/main.go
+++ backend/main.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
 	"os"
-	"strconv"
-
-	// "time"
 
 	"github.com/bzhtux/my-go-gallery/backend/db"
 	"github.com/bzhtux/my-go-gallery/backend/images"
@@ -16,7 +11,7 @@ import (
 )
 
 const (
-	version = "0.0.1"
+	version = "0.0.2"
 )
 
 var (
@@ -29,76 +24,30 @@ var (
 	SMTPPassword = os.Getenv("SMTP_PASSWORD")
 	SMTPHost     = os.Getenv("SMTP_HOST")
 	SMTPPort     = os.Getenv("SMTP_PORT")
-	record_user  = true
 )
 
 func main() {
-	fmt.Println("Starting with version " + version)
+	fmt.Println("\033[32m***********************************")
+	fmt.Println("*** Starting with version " + version + " ***")
+	fmt.Println("***********************************")
 
 	dbConn := db.OpenDB()
 	dbConn.AutoMigrate(&images.Image{})
 	dbConn.AutoMigrate(&users.User{})
 	uid := users.AddDefaultUser(dbConn)
-	fmt.Println(uid)
+	fmt.Println("User Anonymous has ID:", uid)
+	fmt.Println("***********************************\033[0m")
 
 	router := gin.Default()
 	router.MaxMultipartMemory = 16 << 32 // 16 MiB
 
-	router.GET("/user/:uid", func(c *gin.Context) {
-		uid := c.Params.ByName("uid")
-		intVal, _ := strconv.Atoi(uid)
-		user := users.GetUserByID(dbConn, intVal)
-		if user != nil {
-			uFName := user.FirstName
-			uLName := user.LastName
-			uEmail := user.Email
-			uNick := user.NickName
-			c.JSON(http.StatusOK, gin.H{"firstname": uFName, "lastname": uLName, "email": uEmail, "nickname": uNick})
-		}
-		if user == nil {
-			c.JSON(http.StatusNotFound, gin.H{"message": "No Username with id " + uid})
-		}
-
-	})
-
-	router.POST("/user", func(c *gin.Context) {
-		var nu users.User
-		err := c.BindJSON(&nu)
-		if err != nil {
-			log.Println(err)
-			record_user = false
-		}
-		if !record_user {
-			c.JSON(http.StatusBadRequest, gin.H{"message": "Can not register new user: " + nu.FirstName})
-		}
-		if record_user {
-			newUserID := users.AddNewUser(dbConn, nu.FirstName, nu.LastName, nu.Password, nu.Email, nu.NickName)
-			fmt.Println("New User ID:", newUserID)
-			c.JSON(http.StatusOK, gin.H{"userID": newUserID})
-		}
-
-	})
-
-	router.POST("/user/auth", func(c *gin.Context) {
-		var nu users.User
-		err := c.BindJSON(&nu)
-		if err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"Error": err})
-		}
-		dbPass := users.GetPasswordFromEmail(dbConn, nu.Email)
-		if !users.ComparePassword(nu.Password, dbPass) {
-			c.JSON(http.StatusForbidden, gin.H{"Status": "Unauthorized"})
-		}
-		if users.ComparePassword(nu.Password, dbPass) {
-
-			c.JSON(http.StatusOK, gin.H{"Status": "Authorized - creating JWT"})
-		}
-
-	})
-
+	router.GET("/user/:uid", users.GetUserByID)
+	router.POST("/user", users.AddNewUser)
+	router.POST("/user/auth", users.AuthUser)
 	router.POST("/image/upload", images.UploadImage)
 	router.GET("image/:id", images.GetImageByID)
 	router.DELETE("/image/delete/:id", images.DeleteImage)
+	router.DELETE("/user/delete/:uid", users.DeleteUser)
 
 	router.Run(":" + os.Getenv("APP_PORT"))
 
