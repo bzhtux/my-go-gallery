757241a4f08eddd7b50f5e2f437e3d085c19ccbd
Yannick Foeillet <bzhtux@gmail.com>
2022-09-15 15:18:05 +0200

    feat(backend) :) :wrench: Add backend models

---
+++ backend/models/user.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint   `gorm:"primaryKey"`
+	FirstName string `gorm:"not null" json:"firstname"`
+	LastName  string `gorm:"not null" json:"lastname"`
+	Password  string `gorm:"not null" json:"password"`
+	Email     string `gorm:"unique;index" json:"email"`
+	NickName  string `json:"nickname"`
+	Avatar    string
+	Valid     bool
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
