2adcedaaeb1550f8f66e57af4c5980bb3b4d7f4f
Yannick Foeillet <bzhtux@gmail.com>
2022-09-16 17:58:51 +0200

    test(user) log print error for validation error

--- backend/pkg/handlers/AddNewUser.go
+++ backend/pkg/handlers/AddNewUser.go
@@ -14,7 +14,7 @@ func (h Handler) AddNewUser(c *gin.Context) {
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"Status": "Can not register new user:" + err.Error()})
 	} else {
-		log.Println("AdddNewUser Error :", err.Error())
+		log.Println("AddNewUser Error: ", err.Error())
 		result := h.DB.Where("Email = ?", user.Email).First(&user)
 		if result.RowsAffected == 0 {
 			hpass := HashPassword(user.Password)
