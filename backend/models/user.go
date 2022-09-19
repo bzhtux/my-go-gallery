0b7c5f6151bd60eae1c45b7094a1d4b6e8c5113a
Yannick Foeillet <bzhtux@gmail.com>
2022-09-19 10:05:18 +0200

    test(user) test model validation using binding required in struct definition

--- backend/models/user.go
+++ backend/models/user.go
@@ -9,10 +9,10 @@ import (
 type User struct {
 	// gorm.Model
 	ID        uint   `gorm:"primaryKey"`
-	FirstName string `gorm:"not null" json:"firstname"`
-	LastName  string `gorm:"not null" json:"lastname"`
-	Password  string `gorm:"not null" json:"password"`
-	Email     string `gorm:"not null,unique;index" json:"email"`
+	FirstName string `gorm:"not null" json:"firstname" binding:"required"`
+	LastName  string `gorm:"not null" json:"lastname" binding:"required"`
+	Password  string `gorm:"not null" json:"password" binding:"required"`
+	Email     string `gorm:"not null,unique;index" json:"email" binding:"required"`
 	NickName  string `json:"nickname"`
 	Avatar    string
 	Valid     bool
