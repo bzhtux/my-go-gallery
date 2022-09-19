eee223f356c66fe6b576c46a0f2bda2d95ed94d6
Yannick Foeillet <bzhtux@gmail.com>
2022-09-19 16:48:19 +0200

    Debug

--- backend/pkg/handlers/ImageExistsInDB.go
+++ backend/pkg/handlers/ImageExistsInDB.go
@@ -11,8 +11,10 @@ func (h Handler) ImageExistsInDB(filename string) bool {
 	result := h.DB.Where("Name = ?", filename).First(&i)
 	log.Println("Results: ", result)
 	if result.RowsAffected == 0 {
+		log.Println("False (expected 0) => result.RowsAffected: ", result.RowsAffected)
 		return false
 	} else {
+		log.Println("True (image already exists) => result.RowsAffected: ", result.RowsAffected)
 		return true
 	}
 }
