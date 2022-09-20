fb29cdd6edaf4ec7680dbd44c9071391cf9800db
Yannick Foeillet <bzhtux@gmail.com>
2022-09-20 10:00:28 +0200

    feat(images) :wrench: Update upload image if image already exists

--- backend/pkg/handlers/UploadImage.go
+++ backend/pkg/handlers/UploadImage.go
@@ -30,9 +30,10 @@ func (h Handler) UploadImage(c *gin.Context) {
 			}
 		}
 	} else {
-		var i = models.Image{}
-		h.DB.Where("Name = ?", filename.Filename).First(&i)
-		c.JSON(http.StatusConflict, gin.H{"Status": "Image " + filename.Filename + " already exists", "ID": i.ID})
+		r := h.GetImageByName(filename.Filename)
+		// var i = models.Image{}
+		// h.DB.Where("Name = ?", filename.Filename).First(&i)
+		// c.JSON(http.StatusConflict, gin.H{"Status": "Image " + filename.Filename + " already exists", "ID": i.ID})
 	}
 
 }
