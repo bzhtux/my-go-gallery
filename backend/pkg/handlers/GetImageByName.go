8f101282b50cce1fc157c932c9d58f51f6213f88
Yannick Foeillet <bzhtux@gmail.com>
2022-09-20 10:22:15 +0200

    fix(images) :wrench: Fix return code

--- backend/pkg/handlers/GetImageByName.go
+++ backend/pkg/handlers/GetImageByName.go
@@ -13,9 +13,9 @@ func (h Handler) GetImageByName(c *gin.Context) {
 	result := h.DB.Where("Name = ?", filename.Filename).First(&i)
 	if result.RowsAffected == 0 {
 		// No record found with this ID
-		c.JSON(http.StatusOK, gin.H{"Status": "Image " + filename.Filename + " does not exist in DB"})
+		c.JSON(http.StatusNotFound, gin.H{"Status": "Image " + filename.Filename + " does not exist in DB"})
 	} else {
 		// Found record here
-		c.JSON(http.StatusNotFound, gin.H{"Status": "Image with Name " + filename.Filename + " already exists", "ID": i.ID, "Uploaded at": i.CreatedAt})
+		c.JSON(http.StatusConflict, gin.H{"Status": "Image with Name " + filename.Filename + " already exists", "ID": i.ID, "Uploaded at": i.CreatedAt})
 	}
 }
