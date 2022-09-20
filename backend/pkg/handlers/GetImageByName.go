6fd95b43e4064a48575ac099a0bc108aef3a593b
Yannick Foeillet <bzhtux@gmail.com>
2022-09-20 10:17:19 +0200

    feat(images) :wrench: Fix some golang issues

--- backend/pkg/handlers/GetImageByName.go
+++ backend/pkg/handlers/GetImageByName.go
@@ -7,14 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func (h Handler) GetImageByName(filename string) {
+func (h Handler) GetImageByName(c *gin.Context) {
+	filename, _ := c.FormFile("file")
 	var i models.Image
-	result := h.DB.Where("Name = ?", filename).First(&i)
+	result := h.DB.Where("Name = ?", filename.Filename).First(&i)
 	if result.RowsAffected == 0 {
 		// No record found with this ID
-		c.JSON(http.StatusOK, gin.H{"Status": "Image " + filename + " does not exist in DB"})
+		c.JSON(http.StatusOK, gin.H{"Status": "Image " + filename.Filename + " does not exist in DB"})
 	} else {
 		// Found record here
-		c.JSON(http.StatusNotFound, gin.H{"Status": "Image with Name " + filename + " already exists", "ID": i.ID, "Uploaded at": i.CreatedAt})
+		c.JSON(http.StatusNotFound, gin.H{"Status": "Image with Name " + filename.Filename + " already exists", "ID": i.ID, "Uploaded at": i.CreatedAt})
 	}
 }
--- backend/pkg/handlers/UploadImage.go
+++ backend/pkg/handlers/UploadImage.go
@@ -30,7 +30,7 @@ func (h Handler) UploadImage(c *gin.Context) {
 			}
 		}
 	} else {
-		r := h.GetImageByName(filename.Filename)
+		h.GetImageByName(c)
 		// var i = models.Image{}
 		// h.DB.Where("Name = ?", filename.Filename).First(&i)
 		// c.JSON(http.StatusConflict, gin.H{"Status": "Image " + filename.Filename + " already exists", "ID": i.ID})
