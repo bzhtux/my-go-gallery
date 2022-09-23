901820e8b972b7f9734a9d87a15a0ae4901e7094
Yannick Foeillet <bzhtux@gmail.com>
2022-09-23 10:19:10 +0200

    fix(bsa) 🔧 change user id to 1 instead of 0 for recording img

--- bsa/pkg/handlers/RecordImage.go
+++ bsa/pkg/handlers/RecordImage.go
@@ -3,8 +3,8 @@ package handlers
 import "github.com/bzhtux/my-go-gallery/bsa/models"
 
 func (h Handler) RecordImage(img string) bool {
-	// Anonymous context so all images belong to anonumous user that should have id 0
-	image := models.Image{Name: img, UserID: 0}
+	// Anonymous context so all images belong to anonumous user that should have id 1
+	image := models.Image{Name: img, UserID: 1}
 	result := h.DB.Create(&image)
 	if result.RowsAffected != 0 {
 		return true
